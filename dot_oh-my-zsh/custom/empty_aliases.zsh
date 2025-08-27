98e40be54e3d59a9937c69c477475d32a2a8cfc1
re6neck <1027377+re6neck@users.noreply.github.com>
2025-08-27 16:51:44 +0200

    Add starship and aliases

---
+++ dot_oh-my-zsh/custom/empty_aliases.zsh
@@ -0,0 +1,11 @@
+alias vi="nvim"
+
+alias la="lsd -Al --group-dirs=last"
+alias ll="lsd -l --group-dirs=last"
+
+
+alias nbat="bat --style=plain"
+
+alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+
